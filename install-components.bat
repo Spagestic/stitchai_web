565b89828c96e3db13f601a43b36255e7661f061
spagestic <steelmax666@gmail.com>
2025-12-01 05:06:29 +0800

    add Field components and update install script for new dependencies

--- install-components.bat
+++ install-components.bat
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM Components
-set components=accordion alert alert-dialog aspect-ratio avatar badge button breadcrumb card chart checkbox collapsible command context-menu dialog drawer dropdown-menu form hover-card input input-otp label menubar navigation-menu popover progress radio-group scroll-area switch sonner select separator sheet skeleton slider switch table tabs textarea toggle toggle-group tooltip
+set components=accordion alert alert-dialog aspect-ratio avatar badge button breadcrumb card chart checkbox collapsible field command context-menu dialog drawer dropdown-menu form hover-card input input-otp label menubar navigation-menu popover progress radio-group scroll-area switch sonner select separator sheet skeleton slider switch table tabs textarea toggle toggle-group tooltip
 
 REM Loop through each component and install it
 for %%c in (%components%) do (
@@ -16,4 +16,4 @@ echo All components installed successfully!
 @REM install-components.bat
 
 @REM Better to do the following for faster installs:
-@REM bunx --bun shadcn@latest add accordion alert alert-dialog aspect-ratio avatar badge button breadcrumb card chart checkbox collapsible command context-menu dialog drawer dropdown-menu form hover-card input input-otp label menubar navigation-menu popover progress radio-group scroll-area switch sonner select separator sheet skeleton slider switch table tabs textarea toggle toggle-group tooltip
+@REM bunx --bun shadcn@latest add accordion alert alert-dialog aspect-ratio avatar badge button breadcrumb card field chart checkbox collapsible command context-menu dialog drawer dropdown-menu form hover-card input input-otp label menubar navigation-menu popover progress radio-group scroll-area switch sonner select separator sheet skeleton slider switch table tabs textarea toggle toggle-group tooltip

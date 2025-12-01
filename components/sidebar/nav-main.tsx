fa71a387018c6298899744542c1ca4adfbebcd6c
spagestic <steelmax666@gmail.com>
2025-12-01 14:13:43 +0800

    feat: simplify NavMain component by removing unused icons and items

--- components/sidebar/nav-main.tsx
+++ components/sidebar/nav-main.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import {
-  IconCirclePlusFilled,
-  IconChartBar,
-  IconDashboard,
-  IconFolder,
-  IconListDetails,
-  IconUsers,
-} from "@tabler/icons-react";
+import { IconHome, IconUsers } from "@tabler/icons-react";
 
 import {
   SidebarGroup,
@@ -20,27 +13,12 @@ import {
 const navMainItems = [
   {
     title: "Dashboard",
-    url: "#",
-    icon: IconDashboard,
+    url: "/dashboard",
+    icon: IconHome,
   },
   {
-    title: "Lifecycle",
-    url: "#",
-    icon: IconListDetails,
-  },
-  {
-    title: "Analytics",
-    url: "#",
-    icon: IconChartBar,
-  },
-  {
-    title: "Projects",
-    url: "#",
-    icon: IconFolder,
-  },
-  {
-    title: "Team",
-    url: "#",
+    title: "Teams",
+    url: "/teams",
     icon: IconUsers,
   },
 ];
@@ -49,17 +27,6 @@ export function NavMain() {
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
-        <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
         <SidebarMenu>
           {navMainItems.map((item) => (
             <SidebarMenuItem key={item.title}>

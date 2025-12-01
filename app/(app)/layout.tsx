e4055fdb703592a80018d69f84002c8668b80e72
spagestic <steelmax666@gmail.com>
2025-12-01 12:50:03 +0800

    refactor: remove sidebar width style from Layout component

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -6,7 +6,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <SidebarProvider
       style={
         {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
           "--header-height": "calc(var(--spacing) * 12)",
         } as React.CSSProperties
       }

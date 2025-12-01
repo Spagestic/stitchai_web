9194b4156d8d33210394071af53ff25e6b3a0531
spagestic <steelmax666@gmail.com>
2025-12-01 14:15:25 +0800

    feat: update NavDocuments label and remove unused "More" menu item

--- components/sidebar/nav-documents.tsx
+++ components/sidebar/nav-documents.tsx
@@ -50,7 +50,7 @@ export function NavDocuments() {
 
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
-      <SidebarGroupLabel>Documents</SidebarGroupLabel>
+      <SidebarGroupLabel>Recent Designs</SidebarGroupLabel>
       <SidebarMenu>
         {navDocumentsItems.map((item) => (
           <SidebarMenuItem key={item.name}>
@@ -92,12 +92,6 @@ export function NavDocuments() {
             </DropdownMenu>
           </SidebarMenuItem>
         ))}
-        <SidebarMenuItem>
-          <SidebarMenuButton className="text-sidebar-foreground/70">
-            <IconDots className="text-sidebar-foreground/70" />
-            <span>More</span>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
       </SidebarMenu>
     </SidebarGroup>
   );

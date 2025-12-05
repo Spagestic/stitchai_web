f9561a7250c3f186b0beff8ad4e619f9a5949212
spagestic <steelmax666@gmail.com>
2025-12-05 11:50:22 +0800

    refactor: replace useRouter with Link for navigation in CreateHeader

--- components/create/CreateHeader.tsx
+++ components/create/CreateHeader.tsx
@@ -1,25 +1,20 @@
 import { ArrowLeft, RefreshCw } from "lucide-react";
 import { Button } from "../ui/button";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface CreateHeaderProps {
   onRandomize: () => void;
 }
 
 export default function CreateHeader({ onRandomize }: CreateHeaderProps) {
-  const router = useRouter();
-
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => router.back()}
-          className=""
-        >
-          <ArrowLeft />
-        </Button>
+        <Link href="/dashboard">
+          <Button variant="ghost" size="icon" className="">
+            <ArrowLeft />
+          </Button>
+        </Link>
         <h1 className="ml-2 text-md font-bold lg:text-lg">
           Create Your Jersey
         </h1>
